4b0947ef38388e69ce002a3f4e55f4ebd01698fc
VadimLazutin <Tyuiu.LazutinVS@outlook.com>
2025-09-09 16:08:33 +0500

    Task1

---
+++ Tyuiu.LazutinVS.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,10 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        string name = "Вадим";
+        int age = 18;
+        Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " лет!");
+    }
+}
