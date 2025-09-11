70be905d80c95a12fe5be84f86198f1a493e52a3
VadimLazutin <Tyuiu.LazutinVS@outlook.com>
2025-09-11 17:41:43 +0500

    Task2

---
+++ Tyuiu.LazutinVS.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.LazutinVS.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.LazutinVS.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Вадим";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет..., Вадим", res);
+        }
+    }
+}
