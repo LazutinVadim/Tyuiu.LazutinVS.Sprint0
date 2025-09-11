38f21f7ee070dd2db5febba3de1cc48f75292d52
VadimLazutin <Tyuiu.LazutinVS@outlook.com>
2025-09-11 18:49:17 +0500

    Task3

--- Tyuiu.LazutinVS.Sprint0.Task3.V0/Program.cs
+++ Tyuiu.LazutinVS.Sprint0.Task3.V0/Program.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Tyuiu.LazutinVS.Sprint0.Task2.V0.Lib;
-namespace Tyuiu.LazutinVS.Sprint0.Task2.V0
+using Tyuiu.LazutinVS.Sprint0.Task3.V0.Lib;
+namespace Tyuiu.LazutinVS.Sprint0.Task3.V0
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(DataService.GetMessage("Игорь"));
+            Console.WriteLine(DataService.Sum(7, 7));
             Console.ReadKey();
         }
     }
